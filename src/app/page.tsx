898240831411cfcd52d1f765726e078e9cff5b57
Eric Zardo <95961864+ericzardo@users.noreply.github.com>
2025-12-27 00:01:12 -0300

    feat: bucket types, investment logic, new dashboard charts & allocation fixes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,13 @@
-export default function Home() {
-  return (
-    <div>
-    </div>
-  );
+import { redirect } from "next/navigation";
+import { getCurrentUserId } from "@/lib/session";
+
+export default async function RootPage() {
+
+  const id = await getCurrentUserId();
+
+  if (id) {
+    redirect("/dashboard"); 
+  }
+
+  redirect("/auth/login");
 }
